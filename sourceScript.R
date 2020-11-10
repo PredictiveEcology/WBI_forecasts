861f88992f9ac7fc532e657af90ac0b559070020
Ian Eddy <ianmseddy@gmail.com>
2020-11-10 20:23:39 +0000

    new script name

--- sourceScript.R
+++ sourceScript.R
@@ -5,6 +5,6 @@ source('02-packages.R')
 source('03-paths.R')
 source('04-options.R')
 source('05-objects.R')
-source('06-preamble.R')
+source('06-studyArea.R')
 source('07-dataPrep.R')
 source('08-spreadFit.R') #this is out of order, for now
