ba8f0fd65a4da4590a2f1bd55205db5a02b1790f
Ian Eddy <ianmseddy@gmail.com>
2020-11-20 00:41:01 +0000

    first attempt at spreadFit

--- 08c-spreadFit.R
+++ 08c-spreadFit.R
@@ -10,11 +10,11 @@ spreadFitObjects <- list(
   flammableRTM = simDataPrep$flammableRTM,
   studyArea = simDataPrep$studyArea,
   rasterToMatch = simDataPrep$rasterToMatch,
-  fireSense_formula = simDataPrep$fireSense_formula
+  fireSense_formula = simDataPrep$fireSense_formula,
 )
 
 #  lower asymptote, upper asymptote, (inflection point), slope at inflection pt, asymmetry
-#Note this is not from the fireSense_tutorial. No defaults on params..
+#Note this is not from the fireSense_tutorial. No defaults on params yet...
 lowerParams <- c(-16, -16, -16, -16, -16, -16)
 upperParams <- c(32, 32, 32, 32, 32, 32)
 # Spread log function bounds
@@ -29,16 +29,33 @@ upper <- c(0.29, 10, upperParams)
 
 spreadFitParams <- list(
   fireSense_SpreadFit = list(
-    lower = lower,
-    upper = upper
+    'lower' = lower,
+    'upper' = upper,
+    "cores" = if (isRstudioServer()) NULL else cores, #rep("localhost", 40), #cores,
+    "iterDEoptim" = 150,
+    "iterStep" = 150,
+    "debugMode" = FALSE, # DEoptim may spawn many machines via PSOCK --> may be better from cmd line
+    "rescaleAll" = TRUE,
+    "NP" = length(cores),
+    "objFunCoresInternal" = 3L,
+    "maxFireSpread" = 0.3,
+    "objfunFireReps" = 100,
+    "verbose" = TRUE,
+    "trace" = 1,
+    "visualizeDEoptim" = TRUE,
+    #initialpop = if (exists("aa")) aa$member$pop else NULL#[sample(seq_len(NROW(aa$member$pop)), length(cores)),]
+    # "40927e9ca42d33b3", "56769e2b2edfe8ab",  "c3af84b504e99a5d", # This is NWT DEoptim Cache, newer to older
+    "cacheId_DE" = runNamesList()[RunName == runName,
+                                  DEoptimCache], # This is NWT DEoptim Cache
+    "cloudFolderID_DE" = "1kUZczPyArGIIkbl-4_IbtJWBhVDveZFZ",
+    "useCloud_DE" = TRUE
   ))
 
 # Setting up IP's for paralelizing
-cores <- makeIpsForClusters(module = "fireSense",
-                            availableCores = c(9,   9,   9,   9,  19,   9,   9,   9,   8))
-
-
-
+cores <- pemisc::makeIpsForClustersBoreaCloud(module = "fireSense",
+                                              ipEnd = c(97, 189, 220, 106, 217),
+                                              localHostEndIp = 97,
+                                              availableCores = c(24, 25, 25, 13, 13))
 
 #
 devtools::load_all("../fireSenseUtils") #during development
