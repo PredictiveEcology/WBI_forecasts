8f160d6667e0344e27f7801a06f6f9d21e4ffe07
Ian Eddy <ianmseddy@gmail.com>
2020-11-20 20:12:49 +0000

    typo from merge

--- 06-studyArea.R
+++ 06-studyArea.R
@@ -5,7 +5,7 @@ do.call(setPaths, preamblePaths)
 preambleParams <- list(
   WBI_dataprep_studyArea = list(
     "studyAreaName" = studyAreaName,
-    ".useCache" = TRUE
+    ".useCache" = TRUE,
     "historicalFireYears" = 1991:2019 #RIA is now same as everywhere else
   )
 )
