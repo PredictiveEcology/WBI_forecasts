0e1b260a820394bfa055d3ba733b6a346e895c1c
Eliot McIntire <eliotmcintire@gmail.com>
2020-12-20 21:31:56 -0800

    Eliot's change to global.R because taking too long during development

--- 00-global.R
+++ 00-global.R
@@ -1,3 +1,4 @@
+saveOrLoad <- "load"
 switch(peutils::user(),
        "achubaty" = Sys.setenv(R_CONFIG_ACTIVE = "alex"),
        "ieddy" = Sys.setenv(R_CONFIG_ACTIVE = "ian"),
@@ -6,13 +7,22 @@ switch(peutils::user(),
 )
 #Sys.getenv("R_CONFIG_ACTIVE") ## verify
 
+theData <- "all.qs"
 source("01-init.R")
 source("02-packages.R")
 source("03-paths.R")
 source("04-options.R")
 source("05-objects.R")
-source("06-studyArea.R")
-source("07-dataPrep.R")
-#source("08a-ignitionFit.R") ## TODO
-#source("08b-escapeFit.R") ## TODO
+if (!saveOrLoad %in% c("load")) {
+  source("06-studyArea.R")
+  source("07-dataPrep.R")
+  #source("08a-ignitionFit.R") ## TODO
+  #source("08b-escapeFit.R") ## TODO
+  a <- mget(ls())
+  system.time(qs::qsave(x = a, preset = "fast", file = theData, nthreads = 2))
+}
+if (saveOrLoad == "load") {
+  system.time(qs::qload(file = theData, nthreads = 2))
+}
+
 source("08c-spreadFit.R")
