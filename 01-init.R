c28aa8c3e9a97655dca4870ac25f844c0862f0a6
Alex Chubaty <achubaty@for-cast.ca>
2020-12-21 17:25:12 +0000

    fix typo (with prev)

--- 01-init.R
+++ 01-init.R
@@ -7,7 +7,7 @@ library(config)
 cacheDir <- config::get("paths")[["cachedir"]]
 cacheFormat <- config::get("paths")[["cacheformat"]]
 cloudCacheFolderID <- config::get("cloud")[["cachedir"]]
-codechecks <- config::get("codechecks")
+codeChecks <- config::get("codechecks")
 scratchDir <- config::get("paths")[["scratchdir"]]
 studyAreaName <- config::get("studyarea")
 useCloudCache <- config::get("cloud")[["usecloud"]]
