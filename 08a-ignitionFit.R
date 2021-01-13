03ad734c8b8f21b0f30890680c040bcef0927ca2
Ian Eddy <ianmseddy@gmail.com>
2021-01-12 17:45:27 -0800

    ignition updates

--- 08a-ignitionFit.R
+++ 08a-ignitionFit.R
@@ -2,19 +2,27 @@ do.call(setPaths, ignitionFitPaths)
 
 #climVar may become a principal component eventually, hence this format
 climVar <- simDataPrep$fireSense_ignitionCovariates[, 2]
-upper = c(round(max(climVar) * 1.05, digits = 0), 1, 1, 1, 1, 1)
-lower = c(min(climVar) * 0.95, 0, 0, 0, 0, 0)
+
+#This needs work. Need to figure out what these are supposed to look like
+ones <- rep(1, times = ncol(simDataPrep$fireSense_ignitionCovariates) - 3)
+zeroes <- rep(0, times = length(ones))
+upper = c(round(max(climVar) * 1.05, digits = 0), ones)
+lower = c(min(climVar) * 0.95, zeroes)
+names(lower) = names(simDataPrep$fireSense_ignitionCovariates)[2:c(length(upper) + 1)]
+names(upper) = names(lower)
+
+
 
 ignitionFitParams <- list(
   fireSense_IgnitionFit = list(
-    fireSense_ignitionFormula = simDataPrep$fireSense_ignitionFormula
+    fireSense_ignitionFormula = simDataPrep$fireSense_ignitionFormula,
+    ub = list("coef" = upper),
+    lb = list("coef" = lower)
   )
 )
 
 ignitionFitObjects <- list(
   fireSense_ignitionCovariates = simDataPrep$fireSense_ignitionCovariates,
-  upper = upper,
-  lower = lower
 )
 
 
