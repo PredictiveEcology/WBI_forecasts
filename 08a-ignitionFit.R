ee00c11a820659399d497487d0646d6fe114b914
Ian Eddy <ianmseddy@gmail.com>
2021-01-12 16:24:51 -0800

    ignition fit

---
+++ 08a-ignitionFit.R
@@ -0,0 +1,26 @@
+do.call(setPaths, ignitionFitPaths)
+
+#climVar may become a principal component eventually, hence this format
+climVar <- simDataPrep$fireSense_ignitionCovariates[, 2]
+upper = c(round(max(climVar) * 1.05, digits = 0), 1, 1, 1, 1, 1)
+lower = c(min(climVar) * 0.95, 0, 0, 0, 0, 0)
+
+ignitionFitParams <- list(
+  fireSense_IgnitionFit = list(
+    fireSense_ignitionFormula = simDataPrep$fireSense_ignitionFormula
+  )
+)
+
+ignitionFitObjects <- list(
+  fireSense_ignitionCovariates = simDataPrep$fireSense_ignitionCovariates,
+  upper = upper,
+  lower = lower
+)
+
+
+ignitionSim <- simInit(times = list(start = 0, end = 1),
+                       params = ignitionFitParams,
+                       modules = "fireSense_IgnitionFit",
+                       paths = ignitionFitPaths,
+                       objects = ignitionFitObjects)
+ignitionOut <- spades(ignitionSim)
