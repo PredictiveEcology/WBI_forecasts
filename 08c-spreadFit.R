0ca0428f5ed7a4c1980af23c64050af088e9d78e
Ian Eddy <ianmseddy@gmail.com>
2021-01-15 10:40:22 -0800

    fix params and covMinMax in modules

--- 08c-spreadFit.R
+++ 08c-spreadFit.R
@@ -14,13 +14,11 @@ spreadFitObjects <- list(
   rasterToMatch = simDataPrep$rasterToMatch
 )
 
-## so far the minimum PCA value is -9000 (e.g. 9 standard deviations x1000)
-## but the maximum is 27000! In theory these are symmetrical
-## so for safety, -35000 to 35000. need more info
+
 lowerParamsNonAnnual <- rep(-16, times = ncol(simDataPrep$fireSense_nonAnnualSpreadFitCovariates[[1]]) -1)
-lowerParamsAnnual <- c(0, -16)
+lowerParamsAnnual <- c(-16, -16)
 upperParamsNonAnnual <- rep(32, times = length(lowerParamsNonAnnual))
-upperParamsAnnual <- c(1, 32)
+upperParamsAnnual <- c(32, 32)
 lowerParams <- c(lowerParamsAnnual, lowerParamsNonAnnual)
 upperParams <- c(upperParamsAnnual, upperParamsNonAnnual)
 
