436a7c75613fb52daa4bd5aceb65548fed34a101
Eliot McIntire <eliotmcintire@gmail.com>
2021-01-27 17:02:53 +0000

    switch package and path order

---
+++ 03-packages.R
@@ -0,0 +1,20 @@
+Require(c("plyr", "dplyr")) ## ensure plyr loaded before dplyr or there will be problems
+Require("PredictiveEcology/SpaDES.core@development",
+        which = c("Suggests", "Imports", "Depends")) # need Suggests in SpaDES.core
+needed <- reqdPkgs(module = dir(preamblePaths$modulePath), modulePath = preamblePaths$modulePath)
+Require(unlist(needed))
+# Require(c(
+#   "achubaty/amc@development",
+#   "data.table",
+#   "DBI",
+#   "googledrive",
+#   "magrittr",
+#   "parallel",
+#   "qs",
+#   "sf",
+#   "raster",
+#   "PredictiveEcology/fireSenseUtils@development",
+#   "PredictiveEcology/LandR@development",
+#   "PredictiveEcology/SpaDES.core@development",
+#   "PredictiveEcology/pemisc@development"
+# ))
