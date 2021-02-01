7bb6399bb53b6dddc68f2d8c99524a70785321cf
Eliot McIntire <eliotmcintire@gmail.com>
2021-02-01 10:17:51 -0800

    03-packages minimize preloading

--- 03-packages.R
+++ 03-packages.R
@@ -1,8 +1,13 @@
-Require(c("plyr", "dplyr"), upgrade = FALSE) ## ensure plyr loaded before dplyr or there will be problems
-Require("PredictiveEcology/SpaDES.core@development",
+## ensure plyr loaded before dplyr or there will be problems
+Require(c("plyr", "PredictiveEcology/SpaDES.core@development"),
         which = c("Suggests", "Imports", "Depends"), upgrade = FALSE) # need Suggests in SpaDES.core
+
+# This pre-loading is not really necessary -- and it slows everything down simInitAndSpades are skipped with
+#   theData loading step
 #needed <- reqdPkgs(module = dir(preamblePaths$modulePath), modulePath = preamblePaths$modulePath)
 #Require(unique(unlist(needed)), upgrade = FALSE)
+
+
 # Require(c(
 #   "achubaty/amc@development",
 #   "data.table",
