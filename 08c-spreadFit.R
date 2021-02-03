db3352f76283e18e19fff11d6e9dd197e28b8925
Eliot McIntire <eliotmcintire@gmail.com>
2021-02-02 19:09:32 -0800

    eliot mods for Eliot

--- 08c-spreadFit.R
+++ 08c-spreadFit.R
@@ -12,7 +12,7 @@ spreadFitObjects <- list(
   rasterToMatch = simDataPrep$rasterToMatch
 )
 
-extremeVals <- 4
+extremeVals <- 10
 lowerParamsNonAnnual <- rep(-extremeVals, times = ncol(simDataPrep$fireSense_nonAnnualSpreadFitCovariates[[1]]) - 1)
 lowerParamsAnnual <- c(-extremeVals, -extremeVals)
 upperParamsNonAnnual <- rep(extremeVals, times = length(lowerParamsNonAnnual))
@@ -27,26 +27,41 @@ upperParams <- c(upperParamsAnnual, upperParamsNonAnnual)
 # upper <- c(0.29, 10, 10, upperParams)
 
 lower <- c(0.22, 0.001, lowerParams)
-upper <- c(0.28, 10, upperParams)
+upper <- c(0.286, 10, upperParams)
+dfT <- cbind(c("lower", "upper"), t(data.frame(lower, upper)))
+message("Upper and Lower parameter bounds are:")
+Require:::messageDF(dfT)
 
 localHostEndIp <-
   switch(peutils::user(),
          "ieddy" = 97,
          "emcintir" = 189 )
-cores <-  if (peutils::user("ieddy") || peutils::user("emcintir")) {
+cores <-  if (peutils::user("ieddy")) {
   pemisc::makeIpsForNetworkCluster(ipStart = "10.20.0",
                                    ipEnd = c(97, 189, 220, 106, 217),
-                                   localHostEndIp = localHostEndIp,
-                                   availableCores = c(40, 40, 40, 28, 28),
+                                   availableCores = c(46, 46, 46, 28, 28),
                                    availableRAM = c(500, 500, 500, 250, 250),
+                                   localHostEndIp = localHostEndIp,
                                    proc = "cores",
                                    nProcess = length(lower),
                                    internalProcesses = 10,
                                    sizeGbEachProcess = 1)
 } else if (peutils::user("achubaty") && Sys.info()["nodename"] == "forcast02") {
   rep("localhost", 80)
-# } else if (peutils::user("emcintir")) {
-#   rep("localhost", 45)
+} else if (peutils::user("emcintir")) {
+  pemisc::makeIpsForNetworkCluster(ipStart = "10.20.0",
+                                   #ipEnd = c(97, 189, 220, 106, 217),
+                                   #ipEnd = c(97, 189, 220, 217),#, 106, 217, 213, 184),
+                                   #availableCores = c(46, 46, 46, 28),#, 28, 28, 56, 28),
+                                   #availableRAM = c(500, 500, 500, 250),#, 250, 250, 500, 250),
+                                   ipEnd = c(106, 217, 213, 184),
+                                   availableCores = c(20, 24, 46, 28),
+                                   availableRAM = c(250, 250, 500, 250),
+                                   localHostEndIp = localHostEndIp,
+                                   proc = "cores",
+                                   nProcess = length(lower),
+                                   internalProcesses = 10,
+                                   sizeGbEachProcess = 1)
 } else {
   stop("please specify machines to use for spread fit")
 }
@@ -60,18 +75,18 @@ spreadFitParams <- list(
     # "cacheId_DE" = paste0("DEOptim_", studyAreaName), # This is NWT DEoptim Cache
     "cloudFolderID_DE" = cloudCacheFolderID,
     "cores" = cores,
-    "debugMode" = FALSE,
-    "iterDEoptim" = 150,
-    "iterStep" = 150,
+    "debugMode" = T,
+    "iterDEoptim" = if (peutils::user("emcintir")) 300 else 150,
+    "iterStep" = if (peutils::user("emcintir")) 300 else 150,
     "iterThresh" = 192L,
     "lower" = lower,
-    "maxFireSpread" = 0.28,
+    "maxFireSpread" = max(0.28, upper[1]),
     "NP" = length(cores),
     "objFunCoresInternal" = 1L,
     "objfunFireReps" = 100,
     "rescaleAll" = TRUE,
     "trace" = 1,
-    "SNLL_FS_thresh" = NULL, ## NULL means 'autocalibrate' to find suitable threshold value
+    "SNLL_FS_thresh" = if (peutils::user("emcintir")) 505 else NULL,# NULL means 'autocalibrate' to find suitable threshold value
     "upper" = upper,
     "verbose" = TRUE,
     "visualizeDEoptim" = FALSE,
@@ -89,3 +104,11 @@ spreadSim <- simInit(times = list(start = 0, end = 1),
                      paths = spreadFitPaths,
                      objects = spreadFitObjects)
 spreadOut <- spades(spreadSim)
+if (peutils::user("emcintir")) {
+  saveName <- paste0("spreadOut_", Sys.Date(), "_Limit", extremeVals, "_",
+                     spreadFitParams$fireSense_SpreadFit$iterDEoptim, "_",
+                     "SNLL_FS_thresh", spreadFitParams$fireSense_SpreadFit$SNLL_FS_thresh,
+                     "_", SpaDES.core::rndstr(1, 6))
+  saveRDS(spreadOut, file = saveName)
+
+}
