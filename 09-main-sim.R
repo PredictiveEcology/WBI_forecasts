c3caab0fba0ccf8dab79c70d8f1cefcdd6f21a28
Ian Eddy <ianmseddy@gmail.com>
2021-02-05 09:04:55 -0800

    submodule updates

--- 09-main-sim.R
+++ 09-main-sim.R
@@ -1,11 +1,12 @@
 #this will become Main, but for now, i need something fast.
 do.call(setPaths, dynamicPaths)
-dynamicModules <- list("fireSense_dataPrepPredict")
+dynamicModules <- list("fireSense_dataPrepPredict") #Biomass_core, etc will be added
 dynamicObjects <- list(climateComponentsTouse = simDataPrep$climateComponentsToUse,
                        cohortData = simDataPrep$cohortData2011,
                        flammabelRTM = simDataPrep$flammableRTM,
                        landcoverDT = simDataPrep$landcoverDT,
                        nonForest_timeSinceDisturbance = simDataPrep$nonForest_timeSinceDisturbance,
+                       #this is the 2011 TSD - perhaps I should rename it in dataPrepFit to make it explicit?
                        PCAveg = simDataPrep$PCAveg,
                        pixelGroupMap = simDataPrep$pixelGroupMap2011,
                        projectedClimateLayers = simOutPreamble$projectedClimateRasters,
@@ -30,6 +31,3 @@ mySim <- simInit(times = list(start = 2011, end = 2012),
 simOut <- spades(mySim)
 
 
-#Notes
-#castCohortData returns pixelGroup as column... definitely don't need this
-#
