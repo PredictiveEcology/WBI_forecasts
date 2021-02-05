97df610f343b0e36e1bbc0a455e2555df0611e8c
Ian Eddy <ianmseddy@gmail.com>
2021-02-05 08:39:14 -0800

    main sim script update - for development

---
+++ 09-main-sim.R
@@ -0,0 +1,35 @@
+#this will become Main, but for now, i need something fast.
+do.call(setPaths, dynamicPaths)
+dynamicModules <- list("fireSense_dataPrepPredict")
+dynamicObjects <- list(climateComponentsTouse = simDataPrep$climateComponentsToUse,
+                       cohortData = simDataPrep$cohortData2011,
+                       flammabelRTM = simDataPrep$flammableRTM,
+                       landcoverDT = simDataPrep$landcoverDT,
+                       nonForest_timeSinceDisturbance = simDataPrep$nonForest_timeSinceDisturbance,
+                       PCAveg = simDataPrep$PCAveg,
+                       pixelGroupMap = simDataPrep$pixelGroupMap2011,
+                       projectedClimateLayers = simOutPreamble$projectedClimateRasters,
+                       sppEquiv = simDataPrep$sppEquiv,
+                       terrainDT = simDataPrep$terrainDT,
+                       vegComponentsToUse = simDataPrep$vegComponfentsToUse)
+
+dynamicParams <- list(
+  fireSense_dataPrepPredict = list(
+    'fireTimeStep' = 1,
+    'sppEquivCol' = simOutPreamble$sppEquivCol,
+    'whichModulesToPrepare' = c('fireSense_SpreadPredict', 'fireSense_IgnitionPredict'),
+    'missingLCCgroup' = simDataPrep@params$fireSense_dataPrepFit$missingLCCgroup
+  )
+)
+devtools::load_all("../fireSenseUtils")
+mySim <- simInit(times = list(start = 2011, end = 2012),
+                 modules = dynamicModules,
+                 objects = dynamicObjects,
+                 params = dynamicParams,
+                 paths = dynamicPaths)
+simOut <- spades(mySim)
+
+
+#Notes
+#castCohortData returns pixelGroup as column... definitely don't need this
+#
