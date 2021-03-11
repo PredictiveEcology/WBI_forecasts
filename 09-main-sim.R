a7cd5ed63762fb086208664a17220defb9554426
Ian Eddy <ianmseddy@gmail.com>
2021-03-11 14:26:19 -0800

    prediction priorities and ignitionFit bugfix

--- 09-main-sim.R
+++ 09-main-sim.R
@@ -1,10 +1,14 @@
 
 do.call(setPaths, dynamicPaths)
-dynamicModules <- list("fireSense_dataPrepPredict", "fireSense_IgnitionPredict") #Biomass_core, etc will be added
+dynamicModules <- list("fireSense_dataPrepPredict", "fireSense",
+                       "fireSense_IgnitionPredict"
+                       , "fireSense_EscapePredict"
+                       ) #Biomass_core, etc will be added
 dynamicObjects <- list(climateComponentsTouse = simDataPrep$climateComponentsToUse,
                        cohortData = simDataPrep$cohortData2011,
                        flammableRTM = simDataPrep$flammableRTM,
                        fireSense_IgnitionFitted = ignitionOut$fireSense_IgnitionFitted,
+                       fireSense_EscapeFitted = escapeOut$fireSense_EscapeFitted,
                        landcoverDT = simDataPrep$landcoverDT,
                        nonForest_timeSinceDisturbance = simDataPrep$nonForest_timeSinceDisturbance,
                        #this is the 2011 TSD - perhaps I should rename it in dataPrepFit to make it explicit?
@@ -19,14 +23,14 @@ dynamicParams <- list(
   fireSense_dataPrepPredict = list(
     'fireTimeStep' = 1,
     'sppEquivCol' = simOutPreamble$sppEquivCol,
-    'whichModulesToPrepare' = c('fireSense_SpreadPredict', 'fireSense_IgnitionPredict'),
+    'whichModulesToPrepare' = c('fireSense_IgnitionPredict', 'fireSense_EscapePredict'),
     'missingLCCgroup' = simDataPrep@params$fireSense_dataPrepFit$missingLCCgroup
   ),
   fireSense_ignitionPredict = list(
     'rescaleFactor' = 1/simDataPrep@params$fireSense_dataPrepFit$igAggFactor^2
   )
 )
-mainSim <- simInitAndSpades(times = list(start = 2011, end = 2011),
+mainSim <- simInitAndSpades(times = list(start = 2011, end = 2013),
                             modules = dynamicModules,
                             objects = dynamicObjects,
                             params = dynamicParams,
