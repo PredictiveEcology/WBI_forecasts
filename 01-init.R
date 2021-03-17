5b3bcd1278ca2c3e58b3ed9ebcc128e93129b0bb
Alex Chubaty <achubaty@for-cast.ca>
2021-03-17 08:24:29 -0600

    firstRun = FALSE

--- 01-init.R
+++ 01-init.R
@@ -8,7 +8,7 @@ cacheDir <- config::get("paths")[["cachedir"]]
 cacheFormat <- config::get("cacheformat")
 cloudCacheFolderID <- config::get("cloud")[["cachedir"]]
 codeChecks <- config::get("codechecks")
-firstRun <- TRUE ## TODO: restore FALSE
+firstRun <- FALSE ## TODO: restore FALSE
 messagingNumCharsModule <- config::get("messagingNumCharsModule")
 reproducibleAlgorithm <- config::get("reproduciblealgorithm")
 scratchDir <- config::get("paths")[["scratchdir"]]
