05e02f6019a1c54e0b622bb897202acd4de19e48
Alex Chubaty <achubaty@for-cast.ca>
2021-03-26 12:28:28 -0600

    deal with qs data.table weirdness directly when loading from qs file

--- 07b-dataPrep_2011.R
+++ 07b-dataPrep_2011.R
@@ -38,6 +38,15 @@ if (isTRUE(usePrerun)) {
   #  archive::archive_extract(basename(abiomassMaps2011), dirname(abiomassMaps2011))
   #}
   biomassMaps2011 <- loadSimList(fbiomassMaps2011)
+
+  ## TODO: temp until bug in qs resolved
+  biomassMaps2011$cohortData <- as.data.table(biomassMaps2011$cohortData)
+  biomassMaps2011$pixelFateDT <- as.data.table(biomassMaps2011$pixelFateDT)
+  biomassMaps2011$species <- as.data.table(biomassMaps2001$species)
+  biomassMaps2011$speciesEcoregion <- as.data.table(biomassMaps2011$speciesEcoregion)
+  biomassMaps2011$sppEquiv <- as.data.table(biomassMaps2011$sppEquiv)
+  biomassMaps2011$sufficientLight <- as.data.frame(biomassMaps2011$sufficientLight)
+  ## end TODO
 } else {
   biomassMaps2011 <- Cache(
     simInitAndSpades,
--- 07c-dataPrep_fS.R
+++ 07c-dataPrep_fS.R
@@ -44,6 +44,20 @@ if (isTRUE(usePrerun)) {
   #  archive::archive_extract(basename(afSsimDataPrep), dirname(afSsimDataPrep))
   #}
   fSsimDataPrep <- loadSimList(ffSsimDataPrep)
+
+  ## TODO: temporary until bug in qs is fixed
+  fSsimDataPrep$fireSense_escapeCovariates <- as.data.table(fSsimDataPrep$fireSense_escapeCovariates)
+  fSsimDataPrep$fireSense_annualSpreadFitCovariates <- lapply(fSsimDataPrep$fireSense_annualSpreadFitCovariates, as.data.table)
+  fSsimDataPrep$fireBufferedListDT <- lapply(fSsimDataPrep$fireBufferedListDT, as.data.table)
+  fSsimDataPrep$fireSense_nonAnnualSpreadFitCovariates$year2011_year2012_year2013_year2014_year2015_year2016_year2017_year2018_year2019 <- as.data.table(fSsimDataPrep$fireSense_nonAnnualSpreadFitCovariates$year2011_year2012_year2013_year2014_year2015_year2016_year2017_year2018_year2019)
+  fSsimDataPrep$fireSense_nonAnnualSpreadFitCovariates$year2002_year2003_year2004_year2005_year2006_year2007_year2008_year2009_year2010 <- as.data.table(fSsimDataPrep$fireSense_nonAnnualSpreadFitCovariates$year2002_year2003_year2004_year2005_year2006_year2007_year2008_year2009_year2010)
+  fSsimDataPrep$cohortData2011 <- as.data.table(fSsimDataPrep$cohortData2011)
+  fSsimDataPrep$cohortData2001 <- as.data.table(fSsimDataPrep$cohortData2001)
+  fSsimDataPrep$fireSense_ignitionCovariates <- as.data.table(fSsimDataPrep$fireSense_ignitionCovariates)
+  fSsimDataPrep$landcoverDT <- as.data.table(fSsimDataPrep$landcoverDT)
+  fSsimDataPrep$terrainDT <- as.data.table(fSsimDataPrep$terrainDT)
+  fSsimDataPrep$sppEquiv <- as.data.table(fSsimDataPrep$sppEquiv)
+  ## end TODO
 } else {
   fSsimDataPrep <- Cache(
     simInitAndSpades,
