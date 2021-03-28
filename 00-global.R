f1a90888dfaa82203bb7a16e8b12396d5b5979be
Ian Eddy <ianmseddy@gmail.com>
2021-03-28 10:54:42 -0700

    bump utils in global

--- 00-global.R
+++ 00-global.R
@@ -17,7 +17,7 @@ if (FALSE) {
   Require::Require("PredictiveEcology/reproducible@CopyGenericChange (>= 1.2.6.9011)") ## 2021-03-17
   Require::Require("PredictiveEcology/SpaDES.core@rasterToMemoryUpdates (>= 1.0.6.9022)") ## 2021-03-17
 
-  Require::Require("PredictiveEcology/fireSenseUtils@development (>= 0.0.4.9050)", require = FALSE) ## force pemisc and others to be installed correctly
+  Require::Require("PredictiveEcology/fireSenseUtils@development (>= 0.0.4.9052)", require = FALSE) ## force pemisc and others to be installed correctly
 
   Require::Require("PredictiveEcology/SpaDES.install (>= 0.0.2)")
   out <- makeSureAllPackagesInstalled(modulePath = "modules")
