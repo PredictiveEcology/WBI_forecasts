4910362a6fd52935cff060cf6caee447226ce1e4
Alex Chubaty <achubaty@for-cast.ca>
2021-03-28 13:12:37 -0600

    update batch_runs
    @ianmseddy note the commented out ones are those i've started or have completed
    
    can you please start from runs 5 then 4?

--- batch_runs.R
+++ batch_runs.R
@@ -1,41 +1,42 @@
-runName = "BC_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "BC_CCSM4_RCP85_run02"; source("00-global.R")
+#runName = "BC_CCSM4_RCP85_run01"; source("00-global.R") ## 97
+#runName = "BC_CCSM4_RCP85_run02"; source("00-global.R") ## 97
 runName = "BC_CCSM4_RCP85_run03"; source("00-global.R")
 runName = "BC_CCSM4_RCP85_run04"; source("00-global.R")
 runName = "BC_CCSM4_RCP85_run05"; source("00-global.R")
 
-runName = "AB_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "AB_CCSM4_RCP85_run02"; source("00-global.R")
+#runName = "AB_CCSM4_RCP85_run01"; source("00-global.R") ## forcast02
+#runName = "AB_CCSM4_RCP85_run02"; source("00-global.R") ## forcast02
 runName = "AB_CCSM4_RCP85_run03"; source("00-global.R")
 runName = "AB_CCSM4_RCP85_run04"; source("00-global.R")
 runName = "AB_CCSM4_RCP85_run05"; source("00-global.R")
 
-runName = "SK_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "SK_CCSM4_RCP85_run02"; source("00-global.R")
+#runName = "SK_CCSM4_RCP85_run01"; source("00-global.R") ## 102
+#runName = "SK_CCSM4_RCP85_run02"; source("00-global.R") ## 102
 runName = "SK_CCSM4_RCP85_run03"; source("00-global.R")
 runName = "SK_CCSM4_RCP85_run04"; source("00-global.R")
 runName = "SK_CCSM4_RCP85_run05"; source("00-global.R")
 
-runName = "MB_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "MB_CCSM4_RCP85_run02"; source("00-global.R")
-runName = "MB_CCSM4_RCP85_run03"; source("00-global.R")
+#runName = "MB_CCSM4_RCP85_run01"; source("00-global.R") ## forcast02
+#runName = "MB_CCSM4_RCP85_run02"; source("00-global.R") ## 106
+#runName = "MB_CCSM4_RCP85_run03"; source("00-global.R") ## 106
 runName = "MB_CCSM4_RCP85_run04"; source("00-global.R")
 runName = "MB_CCSM4_RCP85_run05"; source("00-global.R")
 
-runName = "NT_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "NT_CCSM4_RCP85_run02"; source("00-global.R")
+#runName = "NT_CCSM4_RCP85_run01"; source("00-global.R") ## 220
+#runName = "NT_CCSM4_RCP85_run02"; source("00-global.R") ## 220
 runName = "NT_CCSM4_RCP85_run03"; source("00-global.R")
 runName = "NT_CCSM4_RCP85_run04"; source("00-global.R")
 runName = "NT_CCSM4_RCP85_run05"; source("00-global.R")
 
-runName = "YT_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "YT_CCSM4_RCP85_run02"; source("00-global.R")
-runName = "YT_CCSM4_RCP85_run03"; source("00-global.R")
-runName = "YT_CCSM4_RCP85_run04"; source("00-global.R")
+#runName = "YT_CCSM4_RCP85_run01"; source("00-global.R") ## forcast02
+#runName = "YT_CCSM4_RCP85_run02"; source("00-global.R") ## forcast02
+#runName = "YT_CCSM4_RCP85_run03"; source("00-global.R") ## 213
+#runName = "YT_CCSM4_RCP85_run04"; source("00-global.R") ## 213
 runName = "YT_CCSM4_RCP85_run05"; source("00-global.R")
 
-runName = "RIA_CCSM4_RCP85_run01"; source("00-global.R")
-runName = "RIA_CCSM4_RCP85_run02"; source("00-global.R")
+## Ian will do RIA
+#runName = "RIA_CCSM4_RCP85_run01"; source("00-global.R") ## 189
+#runName = "RIA_CCSM4_RCP85_run02"; source("00-global.R") ## 189
 runName = "RIA_CCSM4_RCP85_run03"; source("00-global.R")
 runName = "RIA_CCSM4_RCP85_run04"; source("00-global.R")
 runName = "RIA_CCSM4_RCP85_run05"; source("00-global.R")
