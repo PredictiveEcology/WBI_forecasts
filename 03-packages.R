cc6d17d85951a4788c8746a2d07b9fbbc02fd67f
Alex Chubaty <achubaty@for-cast.ca>
2021-03-29 09:34:56 -0600

    update packages

--- 03-packages.R
+++ 03-packages.R
@@ -6,7 +6,7 @@ if (FALSE) {
 }
 
 ## ensure plyr loaded before dplyr or there will be problems
-Require(c("plyr", "PredictiveEcology/SpaDES.core@development (>=1.0.6.9004)"),
+Require(c("plyr", "PredictiveEcology/SpaDES.core@rasterToMemoryUpdates (>= 1.0.6.9022)"),
         which = c("Suggests", "Imports", "Depends"), upgrade = FALSE) # need Suggests in SpaDES.core
 
 Require("jimhester/archive")
