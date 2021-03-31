f5c5423d0cc00010acb83cecd705f7a88f4adb05
Alex Chubaty <achubaty@for-cast.ca>
2021-03-31 09:09:29 -0600

    add post-processing script
    @ianmseddy this is currently a stub
    
    we should discuss next week with @tati-micheletti

---
+++ 10-post-processing.R
@@ -0,0 +1,23 @@
+studyAreas <- c("AB", "BC", "MB", "NT", "SK", "YT", "RIA")
+
+lapply(studyAreas, function(sA) {
+
+  runNames <- paste0(sA, "_CCSM4_RCP85_run", 1:5)
+
+  ##  TODO: annual? decadal?
+  ##  1. summarize mean ignition prob values
+  ##    a. create raster of mean values
+  ##    b. histogram
+  ##    c. `summary(simOut$fireSense_IgnitionPredicted[])`
+  ##
+  ##  2. summarize mean escape prob values
+  ##    a. create raster of mean values
+  ##    b. histogram
+  ##    c. `summary(simOut$fireSense_EscapePredicted[])`
+  ##
+  ##  3. summarize mean spread prob values
+  ##    a. create raster of mean values
+  ##    b. histogram
+  ##    c. `summary(simOut$fireSense_SpreadPredicted[])`
+})
+
