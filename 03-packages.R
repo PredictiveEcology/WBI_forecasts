a2f59df880c353b4af3454903e454e540f10e703
Alex Chubaty <achubaty@for-cast.ca>
2021-08-05 19:53:18 -0600

    install slackr pkg

--- 03-packages.R
+++ 03-packages.R
@@ -4,6 +4,6 @@ Require("PredictiveEcology/SpaDES.core@rasterToMemoryUpdates (>= 1.0.6.9023)",
         which = c("Suggests", "Imports", "Depends"), upgrade = FALSE) # need Suggests in SpaDES.core
 
 Require("achubaty/amc (>= 0.2.0)", require = FALSE, which = c("Suggests", "Imports", "Depends"))
-Require("jimhester/archive", upgrade = FALSE)
+Require(c("jimhester/archive", "slackr"), upgrade = FALSE)
 
 out <- makeSureAllPackagesInstalled(modulePath = "modules")
