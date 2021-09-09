9d9eea813e4faf8fc0ceb8a3cf7eb39784543e0b
Alex Chubaty <achubaty@for-cast.ca>
2021-09-09 10:58:01 -0600

    add CNRM-ESM2-1 as GCM option

---
+++ batch_runs_CNRM-ESM2-1.R
@@ -0,0 +1,131 @@
+##### SSP 245
+
+runName = "BC_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+runName = "AB_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+runName = "SK_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+runName = "MB_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+runName = "NT_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+runName = "YT_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+runName = "RIA_CNRM-ESM2-1_SSP245_run01"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP245_run02"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP245_run03"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP245_run04"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP245_run05"; source("00-global.R") ##
+
+##### SSP 370
+
+runName = "BC_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+runName = "AB_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+runName = "SK_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+runName = "MB_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+runName = "NT_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+runName = "YT_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+runName = "RIA_CNRM-ESM2-1_SSP370_run01"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP370_run02"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP370_run03"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP370_run04"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP370_run05"; source("00-global.R") ##
+
+##### SSP 585
+
+runName = "BC_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "BC_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
+
+runName = "AB_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "AB_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
+
+runName = "SK_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
+
+runName = "MB_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "MB_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
+
+runName = "NT_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "NT_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
+
+runName = "YT_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "YT_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
+
+runName = "RIA_CNRM-ESM2-1_SSP585_run01"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP585_run02"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP585_run03"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP585_run04"; source("00-global.R") ##
+runName = "RIA_CNRM-ESM2-1_SSP585_run05"; source("00-global.R") ##
