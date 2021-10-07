62c66c3d78bbcb85bae39c6326948abac7148a3a
Alex Chubaty <achubaty@for-cast.ca>
2021-10-07 12:08:59 -0600

    climateData script fixes

--- scripts/climate-data-zip.sh
+++ scripts/climate-data-zip.sh
@@ -7,15 +7,16 @@ cd ${DATA_PATH}
 ## NOTE: you must escape spaces, e.g., British\ Columbia
 #PROV=British\ Columbia
 #PROV=Northwest\ Territories\ \&\ Nunavut
-PROV=Alberta
+#PROV=Alberta
+PROV=wholeRIA_20kmbuff_1ArcMinDEM
 
 ## mv files
-mv "${PROV}"/CanESM5_ssp245_* ${DATA_PATH}/future_2011-2100/CanESM5_ssp245/yearly/"${PROV}"/
-mv "${PROV}"/CNRM-ESM2-1_ssp245_* ${DATA_PATH}/future_2011-2100/CNRM-ESM2-1_ssp245/yearly/"${PROV}"/
-mv "${PROV}"/CanESM5_ssp370_* ${DATA_PATH}/future_2011-2100/CanESM5_ssp370/yearly/"${PROV}"/
-mv "${PROV}"/CNRM-ESM2-1_ssp370_* ${DATA_PATH}/future_2011-2100/CNRM-ESM2-1_ssp370/yearly/"${PROV}"/
-mv "${PROV}"/CanESM5_ssp585_* ${DATA_PATH}/future_2011-2100/CanESM5_ssp585/yearly/"${PROV}"
-mv "${PROV}"/CNRM-ESM2-1_ssp585_* ${DATA_PATH}/future_2011-2100/CNRM-ESM2-1_ssp585/yearly/"${PROV}"/
+mv "${PROV}"/CanESM5_ssp245_*Y ${DATA_PATH}/future_2011-2100/CanESM5_ssp245/yearly/"${PROV}"/
+mv "${PROV}"/CNRM-ESM2-1_ssp245_*Y ${DATA_PATH}/future_2011-2100/CNRM-ESM2-1_ssp245/yearly/"${PROV}"/
+mv "${PROV}"/CanESM5_ssp370_*Y ${DATA_PATH}/future_2011-2100/CanESM5_ssp370/yearly/"${PROV}"/
+mv "${PROV}"/CNRM-ESM2-1_ssp370_*Y ${DATA_PATH}/future_2011-2100/CNRM-ESM2-1_ssp370/yearly/"${PROV}"/
+mv "${PROV}"/CanESM5_ssp585_*Y ${DATA_PATH}/future_2011-2100/CanESM5_ssp585/yearly/"${PROV}"
+mv "${PROV}"/CNRM-ESM2-1_ssp585_*Y ${DATA_PATH}/future_2011-2100/CNRM-ESM2-1_ssp585/yearly/"${PROV}"/
 
 ## zip files
 pushd ${DATA_PATH}/future_2011-2100/CanESM5_ssp245/yearly/
