985bd6cc3fe263a830252f67e4d84e453e069277
Alex Chubaty <achubaty@for-cast.ca>
2021-10-24 19:53:39 -0600

    install SpaDES.projects

--- 03-packages.R
+++ 03-packages.R
@@ -2,8 +2,9 @@ Require(c("data.table", "plyr", "pryr")) ## ensure plyr loaded before dplyr or t
 Require("PredictiveEcology/SpaDES.install (>= 0.0.4)")
 Require("PredictiveEcology/SpaDES.core@development (>= 1.0.6.9023)",
         which = c("Suggests", "Imports", "Depends"), upgrade = FALSE) # need Suggests in SpaDES.core
+Require("PredictiveEcology/SpaDES.project@development", require = FALSE)
 
 Require("achubaty/amc (>= 0.2.0)", require = FALSE, which = c("Suggests", "Imports", "Depends"))
-Require(c("jimhester/archive", "slackr"), upgrade = FALSE)
+Require(c("archive", "slackr"), upgrade = FALSE)
 
 out <- makeSureAllPackagesInstalled(modulePath = "modules")
