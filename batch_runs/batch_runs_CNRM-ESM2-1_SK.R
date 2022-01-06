03ad050adb2ed9a7b0d44a77d92f011ca0ca0aa8
Alex Chubaty <achubaty@for-cast.ca>
2022-01-06 14:55:10 -0700

    update batch scripts; split per studyArea

---
+++ batch_runs/batch_runs_CNRM-ESM2-1_SK.R
@@ -0,0 +1,13 @@
+##### SSP 370
+runName = "SK_CNRM-ESM2-1_SSP370_run01"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run02"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run03"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run04"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP370_run05"; source("00b-global_sim.R") ##
+
+##### SSP 585
+runName = "SK_CNRM-ESM2-1_SSP585_run01"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run02"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run03"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run04"; source("00b-global_sim.R") ##
+runName = "SK_CNRM-ESM2-1_SSP585_run05"; source("00b-global_sim.R") ##
