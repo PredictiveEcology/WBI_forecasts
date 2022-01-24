87e0e53f3e37256e748f8ebdd507962364876136
Alex Chubaty <achubaty@for-cast.ca>
2022-01-24 14:07:10 -0700

    more tweaks/fixes

--- 01-packages.R
+++ 01-packages.R
@@ -2,9 +2,13 @@ if (file.exists(".Renviron")) readRenviron(".Renviron")
 
 pkgDir <- Sys.getenv("PRJ_PKG_DIR")
 if (!nzchar(pkgDir)) {
-  pkgDir <- file.path("packages", version$platform, paste0(version$major, ".",
-                                                           strsplit(version$minor, "[.]")[[1]][1]))
+  pkgDir <- "packages" ## default: use subdir within project directory
 }
+pkgDir <- normalizePath(
+  file.path(pkgDir, version$platform, paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])),
+  winslash = "/",
+  mustWork = FALSE
+)
 
 if (!dir.exists(pkgDir)) {
   dir.create(pkgDir, recursive = TRUE)
