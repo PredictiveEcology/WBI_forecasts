380d20af175b02a0874f9ff37fea619a9da6a6c7
Alex Chubaty <achubaty@for-cast.ca>
2022-01-27 15:00:50 -0700

    tweak spatial pkg install

--- 01-packages.R
+++ 01-packages.R
@@ -22,11 +22,12 @@ if (!require("Require", quietly = TRUE)) {
   library(Require)
 }
 
+.spatialPkgs <- c("lwgeom", "rgdal", "rgeos", "sf", "sp", "raster", "terra")
+
 Require("PredictiveEcology/SpaDES.install@development")
-installSpaDES()
+installSpaDES(dontUpdate = .spatialPkgs)
 
 if (FALSE) {
-  .spatialPkgs <- c("lwgeom", "rgdal", "rgeos", "sf", "sp", "raster", "terra")
   installSpatialPackages()
   #install.packages(c("raster", "terra"), repos = "https://rspatial.r-universe.dev")
   sf::sf_extSoftVersion() ## want GEOS 3.9.0, GDAL 3.2.1, PROJ 7.2.1
