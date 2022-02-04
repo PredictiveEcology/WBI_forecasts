ea65431d9c43fcb50bee78951b8bf16dd50d3bdc
Alex Chubaty <achubaty@for-cast.ca>
2022-02-04 11:56:53 -0700

    minor tweaks to batch scripts

---
+++ batch_runs/batch_runs_wbi.R
@@ -0,0 +1,17 @@
+#source("batch_runs/batch_runs_CanESM5_AB.R") ## psedotsuga
+source("batch_runs/batch_runs_CNRM-ESM2-1_AB.R")
+
+source("batch_runs/batch_runs_CanESM5_BC.R")
+source("batch_runs/batch_runs_CNRM-ESM2-1_BC.R")
+
+source("batch_runs/batch_runs_CanESM5_SK.R")
+source("batch_runs/batch_runs_CNRM-ESM2-1_SK.R")
+
+source("batch_runs/batch_runs_CanESM5_MB.R")
+source("batch_runs/batch_runs_CNRM-ESM2-1_MB.R")
+
+source("batch_runs/batch_runs_CanESM5_YT.R")
+source("batch_runs/batch_runs_CNRM-ESM2-1_YT.R")
+
+#source("batch_runs/batch_runs_CanESM5_NT.R") ## pseudotsuga
+#source("batch_runs/batch_runs_CNRM-ESM2-1_NT.R") ## psedotsuga
