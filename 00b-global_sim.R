be6058141882682112d94bc1c60e1fbd81975083
Alex Chubaty <achubaty@for-cast.ca>
2022-02-11 23:05:50 -0700

    decrease delayStart for batch sims

--- 00b-global_sim.R
+++ 00b-global_sim.R
@@ -8,8 +8,8 @@ source("04-options.R")
 source("05-google-ids.R")
 
 if (delayStart > 0 & run == 1) {
-  message(crayon::green("\nStaggered job start: delaying", runName, "by", delayStart, "minutes."))
-  Sys.sleep(delayStart*60)
+  message(crayon::green("\nStaggered job start: delaying", runName, "by", delayStart/10, "minutes."))
+  Sys.sleep(delayStart*6)
 }
 
 source("06-studyArea.R")
@@ -17,8 +17,9 @@ source("07a-dataPrep_2001.R")
 source("07b-dataPrep_2011.R")
 source("07c-dataPrep_fS.R")
 
-source("08a-ignitionFit.R")
+source("08a-ignitionFit.R") ## rerun ignition fit AB, BC, SK
 source("08b-escapeFit.R")
+usePrerun = TRUE; reupload = FALSE
 source("08c-spreadFit.R")
 
 source("09-main-sim.R")
