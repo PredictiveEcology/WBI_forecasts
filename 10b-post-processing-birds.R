362d12db5a34e2c9b5acd9480643767f3b3a67fc
Tati Micheletti <tati.micheletti@gmail.com>
2022-02-16 03:16:37 -0700

    Added RTM and SA to posthoc

--- 10b-post-processing-birds.R
+++ 10b-post-processing-birds.R
@@ -1,18 +1,37 @@
+#######################################
+#                                     #
+#    P O S T H O C     B I R D S      #
+#                                     #
+#######################################
+
 moduleDir <- "modules"
-usePrerun <- TRUE
 
 source("01-packages.R")
-
 source("02-init.R")
 source("03-paths.R")
 source("04-options.R")
 source("05-google-ids.R")
+source("R/makeStudyArea.R")
 
-if (delayStart > 0 & run == 1) {
-  message(crayon::green("\nStaggered job start: delaying", runName, "by", delayStart, "minutes."))
-  Sys.sleep(delayStart*60)
+Require("raster")
+Require("sf")
+
+do.call(setPaths, posthocPaths)
+
+# RTM
+
+pathRTM <- file.path(Paths$inputPath, paste0(studyAreaName, "_rtm.tif"))
+
+if (file.exists(pathRTM)){
+  rasterToMatch <- raster(pathRTM)
+} else stop("RTM doesn't exist. Please run script 'source('06-studyArea.R')'")
+
+# STUDY AREA
+
+pathSA <- file.path(Paths[["inputPath"]], paste0(studyAreaName, "_SA.qs"))
+
+if (file.exists(pathSA)){
+  SA <- qs::qread(pathSA)
+} else {
+  SA <- makeStudyArea(studyAreaName)
 }
-
-source("06-studyArea.R")
-#source("07a-dataPrep_2001.R")
-source("07b-dataPrep_2011.R")
