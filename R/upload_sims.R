5a4cb35df0abcc20a682b610d858210664daa7ca
Alex Chubaty <achubaty@for-cast.ca>
2022-02-21 10:09:09 -0700

    update upload script

--- R/upload_sims.R
+++ R/upload_sims.R
@@ -1,7 +1,19 @@
+#source("01-packages.R")
+
+library(Require)
+library(reproducible)
+library(googledrive)
+
+source("05-google-ids.R")
+
 files2upload <- c(
   list.files("outputs", "(CanESM5|CNRM-ESM2-1).*[.]tar.gz$", full.names = TRUE)
 )
-lapply(files2upload, function(tarball) {
-  retry(quote(drive_put(media = tarball, path = unique(as_id(gid_results)), name = basename(tarball))),
+
+m <- which(basename(files2upload) == "AB_CanESM5_SSP370_run01.tar.gz")
+lapply(files2upload[-c(1:m)], function(tarball) {
+  studyAreaName <- substr(basename(tarball), 1, 2)
+  gid_results <- gdriveSims[studyArea == studyAreaName & simObject == "results", gid]
+    retry(quote(drive_put(media = tarball, path = unique(as_id(gid_results)), name = basename(tarball))),
         retries = 5, exponentialDecayBase = 2)
 })
