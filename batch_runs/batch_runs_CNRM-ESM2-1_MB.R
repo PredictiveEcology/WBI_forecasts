534af5b2c5d2c74892ea028af4acc9c846b21e95
Alex Chubaty <achubaty@for-cast.ca>
2022-03-01 12:33:07 -0700

    ensure LandR.CS is used for Biomass_core sims
    need to pass `growthAndMortalityDrivers` param

--- batch_runs/batch_runs_CNRM-ESM2-1_MB.R
+++ batch_runs/batch_runs_CNRM-ESM2-1_MB.R
@@ -1,8 +1,8 @@
 ##### SSP 370
-# runName = "MB_CNRM-ESM2-1_SSP370_run01"; source("00b-global_sim.R") ##
-# runName = "MB_CNRM-ESM2-1_SSP370_run02"; source("00b-global_sim.R") ##
-# runName = "MB_CNRM-ESM2-1_SSP370_run03"; source("00b-global_sim.R") ##
-# runName = "MB_CNRM-ESM2-1_SSP370_run04"; source("00b-global_sim.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run01"; source("00b-global_sim.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run02"; source("00b-global_sim.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run03"; source("00b-global_sim.R") ##
+runName = "MB_CNRM-ESM2-1_SSP370_run04"; source("00b-global_sim.R") ##
 runName = "MB_CNRM-ESM2-1_SSP370_run05"; source("00b-global_sim.R") ##
 
 ##### SSP 585
--- batch_runs/batch_runs_wbi.R
+++ batch_runs/batch_runs_wbi.R
@@ -1,14 +1,14 @@
-source("batch_runs/batch_runs_CanESM5_AB.R")
-source("batch_runs/batch_runs_CNRM-ESM2-1_AB.R")
+#source("batch_runs/batch_runs_CanESM5_AB.R")
+#source("batch_runs/batch_runs_CNRM-ESM2-1_AB.R")
 
-source("batch_runs/batch_runs_CanESM5_BC.R")
-source("batch_runs/batch_runs_CNRM-ESM2-1_BC.R")
+#source("batch_runs/batch_runs_CanESM5_BC.R")
+#source("batch_runs/batch_runs_CNRM-ESM2-1_BC.R")
 
 source("batch_runs/batch_runs_CanESM5_SK.R")
 source("batch_runs/batch_runs_CNRM-ESM2-1_SK.R")
 
-source("batch_runs/batch_runs_CanESM5_MB.R")
-source("batch_runs/batch_runs_CNRM-ESM2-1_MB.R")
+#source("batch_runs/batch_runs_CanESM5_MB.R")
+#source("batch_runs/batch_runs_CNRM-ESM2-1_MB.R")
 
 source("batch_runs/batch_runs_CanESM5_YT.R") ## pseudotsuga
 source("batch_runs/batch_runs_CNRM-ESM2-1_YT.R") ## pseudotsuga
