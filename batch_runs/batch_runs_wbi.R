a47a2cef0272203e656228bc5b5891e25851a741
Alex Chubaty <achubaty@for-cast.ca>
2022-03-02 15:28:15 -0700

    update batch runs scripts

--- batch_runs/batch_runs_wbi.R
+++ batch_runs/batch_runs_wbi.R
@@ -15,18 +15,3 @@ source("batch_runs/batch_runs_CNRM-ESM2-1_MB.R")
 
 source("batch_runs/batch_runs_CanESM5_NT.R") ## pseudotsuga
 source("batch_runs/batch_runs_CNRM-ESM2-1_NT.R") ## psedotsuga
-
-# upload files --------------------------------------------------------------------------------
-
-runName <- "AB_CanESM5_SSP370_run01"
-studyAreaName <- ""
-source("05-google-ids.R")
-
-files2upload <- c(
-  list.files("outputs", "(CanESM5|CNRM-ESM2-1).*[.]tar.gz$", full.names = TRUE)
-)
-f <- which(basename(files2upload) == "AB_CanESM5_SSP370_run01.tar.gz")
-lapply(files2upload[-c(1:f)], function(tarball) {
-  retry(quote(drive_put(media = tarball, path = unique(as_id(gid_results)), name = basename(tarball))),
-        retries = 5, exponentialDecayBase = 2)
-})
