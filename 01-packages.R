173b6667b7397e328062f0561541d19940e17a9c
Alex Chubaty <achubaty@for-cast.ca>
2022-03-14 16:01:33 -0600

    tweak pkg workflow; update fireSense_summary

--- 01-packages.R
+++ 01-packages.R
@@ -1,28 +1,11 @@
-if (file.exists(".Renviron")) readRenviron(".Renviron")
-
-pkgDir <- Sys.getenv("PRJ_PKG_DIR")
-if (!nzchar(pkgDir)) {
-  pkgDir <- "packages" ## default: use subdir within project directory
-}
-pkgDir <- normalizePath(
-  file.path(pkgDir, version$platform, paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1])),
-  winslash = "/",
-  mustWork = FALSE
-)
-
-if (!dir.exists(pkgDir)) {
-  dir.create(pkgDir, recursive = TRUE)
-}
-
-.libPaths(pkgDir)
-message("Using libPaths:\n", paste(.libPaths(), collapse = "\n"))
+source("01a-packages-libPath.R")
 
 if (!require("Require", quietly = TRUE)) {
   install.packages("Require")
   library(Require)
 }
 
-.spatialPkgs <- c("lwgeom", "rgdal", "rgeos", "sf", "sp", "raster", "terra")
+.spatialPkgs <- c("lwgeom", "rgdal", "rgeos", "sf", "sp", "stars", "raster", "terra")
 
 Require("PredictiveEcology/SpaDES.install@development")
 
