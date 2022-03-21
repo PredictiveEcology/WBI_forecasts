dd6ed2826cfde5c31ed083533ff19c2e070befd3
Alex Chubaty <achubaty@for-cast.ca>
2022-03-21 12:04:02 -0600

    fix token check in 04-options

--- 04-options.R
+++ 04-options.R
@@ -55,7 +55,7 @@ httr::set_config(httr::config(http_version = 0))
 httr::timeout(seconds = 10)
 
 token <- Require::normPath(list.files(".", "western-boreal-initiative-.*[.]json")[1])
-haveToken <- isTRUE(length(token) == 1)
+haveToken <- all(isTRUE(length(token) == 1), !is.na(token))
 
 if (haveToken) {
   drive_auth(path = token)
